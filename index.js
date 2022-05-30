1d487ee33bbccd3cee62c218c8ca3ee4af04d8a1
Chigozie Ugwuede <sparkyglobe@gmail.com>
2022-05-30 20:19:58 +0100

    implemented the mongodb connection

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
 const config = require('config');
+const mongoose = require('mongoose');
 
 const app = express();
+
+const db = config.get("db");
+
+mongoose.connect(db,(err)=>{
+    if(err) throw new Error(err);
+    console.log('db connected...');
+})
 
 const PORT = config.get('port') || 5001;
 
