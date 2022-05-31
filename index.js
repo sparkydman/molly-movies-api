45ff85b85065ebeb4fec82b4a2f3fa6dd7eb6c73
Chigozie Ugwuede <sparkyglobe@gmail.com>
2022-05-31 12:56:22 +0100

    implemented logger

--- index.js
+++ index.js
@@ -1,16 +1,18 @@
+require('express-async-errors');
 const express = require('express');
 const config = require('config');
 const mongoose = require('mongoose');
+const logger = require('./src/log/logger');
 
 const app = express();
 
-const db = config.get("db");
+const db = config.get('db');
 
-mongoose.connect(db,(err)=>{
-    if(err) throw new Error(err);
-    console.log('db connected...');
-})
+mongoose.connect(db, (err) => {
+  if (err) throw new Error(err);
+  console.log('db connected...');
+});
 
 const PORT = config.get('port') || 5001;
 
-app.listen(PORT, () => console.log(`application is listening on ${PORT}`));
+app.listen(PORT, () => logger.info(`application is listening on ${PORT}`));
---
+++ src/log/logger.js
@@ -0,0 +1,34 @@
+const winston = require('winston');
+const config = require('config');
+
+const options = {
+  db: config.get('errorDB'),
+  level: 'error',
+  name: 'errorLog',
+  collection: 'errors',
+  tryReconnect: true,
+  capped: true,
+  cappedSize: 1024 * 10,
+  cappedMax: 3,
+};
+
+const logger = winston.createLogger({
+  level: 'info',
+  transports: [
+    new winston.transports.Console({ format: winston.format.json() }),
+  ],
+  rejectionHandlers: [
+    new winston.transports.Console({ format: winston.format.json() }),
+  ],
+  exceptionHandlers: [
+    new winston.transports.Console({ format: winston.format.json() }),
+  ],
+});
+
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({ format: winston.format.json() })
+  );
+}
+
+module.exports = logger;
